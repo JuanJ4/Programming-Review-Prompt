9feb02b08c188475e792b42bffdec66d073b611f
Juan J <carlosjjf4@gmail.com>
2023-04-13 23:06:40 -0700

    Update ucla_assesment.ps1

--- ucla_assesment.ps1
+++ ucla_assesment.ps1
@@ -1,12 +1,3 @@
-# https://woozy-helenium-b71.notion.site/Programming-Review-Prompt-e1bcb286c89e4687a603b921e53e368a
-# - To be included as a valid entry the following must be adhered to:
-#     - The solution must be able to run on the interviewers machine at the time of the interview.
-#     - All solutions must be submitted with a public facing git repository.
-#         - The report must be clearly labeled in the repository
-#     - During the interview you will be asked to present the report
-#     - All data processing from the API and into the CSV must be in Powershell
-#         - Nuget and additional tools are allowed
-
 $url = "https://datausa.io/api/data?drilldowns=State&measures=Population"
 
 $headers = @{
